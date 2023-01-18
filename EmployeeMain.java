43f86808205def24b66c4801547532ba569445aa
nesrindemir <95645223+nesrindemir@users.noreply.github.com>
2023-01-18 12:10:50 +0300

    Create EmployeeMain.java

---
+++ EmployeeMain.java
@@ -0,0 +1,8 @@
+public class EmployeeMain {
+    public static void main(String[] args) {
+        Employee employee =new Employee("Ali",8500,45,2021);
+        employee.toStringEmployee();
+
+
+    }
+}
